67ad9a3fc486ddd10100371e271bc387a9a1aa56
Manoj Kumar <83910784+manoj6708@users.noreply.github.com>
2021-06-24 17:04:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,3 +23,38 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+<script type="text/javascript">//<![CDATA[ 
+(function() {
+    var configuration = {
+    "token": "7b4b8c111399269aa0ad4c7036ff926f",
+    "excludeDomains": [
+        "manoj6708.github.io/ManojDhimanYT/"
+    ],
+    "capping": {
+        "limit": 5,
+        "timeout": 24
+    },
+    "entryScript": {
+        "type": "timeout",
+        "timeout": 3000,
+        "capping": {
+            "limit": 5,
+            "timeout": 24
+        }
+    },
+    "exitScript": {
+        "enabled": true
+    },
+    "popUnder": {
+        "enabled": true
+    }
+};
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = '//cdn.shorte.st/link-converter.min.js';
+    script.onload = script.onreadystatechange = function () {var rs = this.readyState; if (rs && rs != 'complete' && rs != 'loaded') return; shortestMonetization(configuration);};
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(script, entry);
+})();
+//]]></script>
